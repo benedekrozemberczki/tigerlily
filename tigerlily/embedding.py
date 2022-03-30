718674343d020cbcb2b170579e29203b81177f84
Rozemberczki <kmdb028@astrazeneca.net>
2022-03-30 18:07:50 +0100

    reformat

--- tigerlily/embedding.py
+++ tigerlily/embedding.py
@@ -3,24 +3,25 @@ from scipy.sparse import coo_matrix
 from sklearn.decomposition import NMF
 import numpy as np
 
+
 class EmbeddingMachine:
-    
-    def __init__(self, seed: int=42, dimensions: int=128, max_iter: int=20):
+    def __init__(self, seed: int = 42, dimensions: int = 128, max_iter: int = 20):
         self.seed = seed
         self.dimensions = dimensions
         self.max_iter = max_iter
-        
+
     def generate_mappings(self, pagerank_scores):
         node_1_mapping = {node: i for i, node in enumerate(set(pagerank_scores["node_1"].values.tolist()))}
         node_2_mapping = {node: i for i, node in enumerate(set(pagerank_scores["node_2"].values.tolist()))}
         pagerank_scores["node_1_num"] = pagerank_scores["node_1"].map(lambda x: node_1_mapping[x])
-        pagerank_scores["node_2_num"] = pagerank_scores["node_2"].map(lambda x: node_2_mapping[x]) 
-        mat = coo_matrix((pagerank_scores["score"], (pagerank_scores["node_1_num"], pagerank_scores["node_2_num"])),
-                         shape=(len(node_1_mapping), len(node_2_mapping)))
+        pagerank_scores["node_2_num"] = pagerank_scores["node_2"].map(lambda x: node_2_mapping[x])
+        mat = coo_matrix(
+            (pagerank_scores["score"], (pagerank_scores["node_1_num"], pagerank_scores["node_2_num"])),
+            shape=(len(node_1_mapping), len(node_2_mapping)),
+        )
 
         return pagerank_scores, mat
-        
-        
+
     def fit(self, pagerank_scores: pd.DataFrame):
         assert "node_1" in pagerank_scores
         assert "node_2" in pagerank_scores
@@ -28,31 +29,27 @@ class EmbeddingMachine:
         pagerank_scores, pagerank_mat = self.generate_mappings(pagerank_scores)
         model = NMF(n_components=self.dimensions)
         raw_embedding = model.fit_transform(pagerank_mat)
-        raw_embedding = (raw_embedding-raw_embedding.mean(0))/raw_embedding.std(0)
-        embedding = pd.DataFrame(raw_embedding, columns =["emb_"+str(i) for i in range(self.dimensions)])
-        pagerank_scores = pagerank_scores[["node_1","node_1_num"]].drop_duplicates(ignore_index=True).sort_values("node_1_num")
-        embedding = pd.concat([pagerank_scores, embedding],axis=1)
+        raw_embedding = (raw_embedding - raw_embedding.mean(0)) / raw_embedding.std(0)
+        embedding = pd.DataFrame(raw_embedding, columns=["emb_" + str(i) for i in range(self.dimensions)])
+        pagerank_scores = (
+            pagerank_scores[["node_1", "node_1_num"]].drop_duplicates(ignore_index=True).sort_values("node_1_num")
+        )
+        embedding = pd.concat([pagerank_scores, embedding], axis=1)
         del embedding["node_1_num"]
         self.embedding = embedding.rename(columns={"node_1": "node_id"})
         return self.embedding
-    
-
 
     def create_features(self, target, feature_definition):
         self.embedding = self.embedding.set_index("node_id")
 
-        drug_features_left = target[["drug_1"]].rename(columns={"drug_1":"node_id"})
+        drug_features_left = target[["drug_1"]].rename(columns={"drug_1": "node_id"})
         drug_features_left = drug_features_left.set_index("node_id").join(self.embedding)
         drug_features_left = np.array(drug_features_left.reset_index(drop=True))
-        
-        drug_features_right = target[["drug_2"]].rename(columns={"drug_2":"node_id"})
+
+        drug_features_right = target[["drug_2"]].rename(columns={"drug_2": "node_id"})
         drug_features_right = drug_features_right.set_index("node_id").join(self.embedding)
         drug_features_right = np.array(drug_features_right.reset_index(drop=True))
-        
+
         drug_pair_features = feature_definition(drug_features_left, drug_features_right)
         self.embedding = self.embedding.reset_index()
         return drug_pair_features
-        
-        
-        
-        
--- tigerlily/pagerank.py
+++ tigerlily/pagerank.py
@@ -7,34 +7,32 @@ import pyTigerGraph as tg
 from typing import List
 from tqdm.notebook import tqdm
 
+
 class PersonalizedPageRankMachine:
-    
-    def __init__(self, host: str, graphname: str, secret: str, password:str):
+    def __init__(self, host: str, graphname: str, secret: str, password: str):
         self._host = host
         self._graphname = graphname
         self._secret = secret
         self._password = password
-        
+
     def connect(self):
-        token_getter = tg.TigerGraphConnection(host=self._host,
-                                               graphname=self._graphname)
-        
+        token_getter = tg.TigerGraphConnection(host=self._host, graphname=self._graphname)
+
         token = token_getter.getToken(self._secret, "12000")[0]
 
-        self.connection = tg.TigerGraphConnection(host=self._host,
-                                                  graphname=self._graphname,
-                                                  password=self._password,
-                                                  apiToken=token)
-        
+        self.connection = tg.TigerGraphConnection(
+            host=self._host, graphname=self._graphname, password=self._password, apiToken=token
+        )
+
     def purge_graph(self):
         self.connection.delVertices("drug")
         self.connection.delVertices("gene")
-        
-    def upload_relationship(self, edges, source:str, target: str, edge_type: str="interacts"):
+
+    def upload_relationship(self, edges, source: str, target: str, edge_type: str = "interacts"):
         sub_edges = edges[(edges["type_1"] == source) & (edges["type_2"] == target)]
-        sub_edges = [(edge[0], edge[1], {}) for edge in sub_edges[["node_1","node_2"]].values.tolist()]
+        sub_edges = [(edge[0], edge[1], {}) for edge in sub_edges[["node_1", "node_2"]].values.tolist()]
         self.connection.upsertEdges(source, edge_type, target, sub_edges)
-        
+
     def upload_graph(self, new_graph: bool, edges: pd.DataFrame):
         assert "type_1" in edges.columns and "type_2" in edges.columns
         assert "node_1" in edges.columns and "node_2" in edges.columns
@@ -42,54 +40,54 @@ class PersonalizedPageRankMachine:
         self.upload_relationship(edges, "drug", "gene", "interacts")
         self.upload_relationship(edges, "gene", "gene", "interacts")
         self.upload_relationship(edges, "gene", "drug", "interacts")
-        
+
     def install_query(self, url: str):
         script = requests.get(url).text
         script = script.replace("CREATE QUERY", "CREATE OR REPLACE QUERY")
         self.connection.gsql(script)
         self.connection.gsql("INSTALL QUERY ALL")
-        
-    def personalized_pagerank(self,
-                                  node_id: str,
-                                  node_type: str="drug",
-                                  edge_type: str="interacts",
-                                  print_accum: bool=True,
-                                  damping: float=0.5,
-                                  iterations: int=100,
-                                  top_k: int=100):
-        
+
+    def personalized_pagerank(
+        self,
+        node_id: str,
+        node_type: str = "drug",
+        edge_type: str = "interacts",
+        print_accum: bool = True,
+        damping: float = 0.5,
+        iterations: int = 100,
+        top_k: int = 100,
+    ):
+
         parameters = dict()
-        parameters["source"] = [{"type":node_type,"id":node_id}]
+        parameters["source"] = [{"type": node_type, "id": node_id}]
         parameters["e_type"] = "interacts"
-        parameters["print_accum"]  = print_accum
+        parameters["print_accum"] = print_accum
         parameters["damping"] = damping
-        parameters["iter"]  = iterations
+        parameters["iter"] = iterations
         parameters["top_k"] = top_k
-        query = "RUN QUERY tg_pagerank_pers("+json.dumps(parameters)+")"
+        query = "RUN QUERY tg_pagerank_pers(" + json.dumps(parameters) + ")"
         response = self.connection.gsql(query)
         response = json.loads(response)
         return response
-    
-    def get_personalized_pagerank(self,
-                                  node_ids: List,
-                                  edge_type: str="interacts",
-                                  print_accum: bool=True,
-                                  damping: float=0.5,
-                                  iterations: int=100,
-                                  top_k: int=100):
+
+    def get_personalized_pagerank(
+        self,
+        node_ids: List,
+        edge_type: str = "interacts",
+        print_accum: bool = True,
+        damping: float = 0.5,
+        iterations: int = 100,
+        top_k: int = 100,
+    ):
         all_scores = []
         for node_id in tqdm(node_ids):
-            scores = self.personalized_pagerank(node_id["v_id"],
-                                                    node_id["v_type"],
-                                                    edge_type,
-                                                    print_accum,
-                                                    damping,
-                                                    iterations,
-                                                    top_k)
+            scores = self.personalized_pagerank(
+                node_id["v_id"], node_id["v_type"], edge_type, print_accum, damping, iterations, top_k
+            )
             scores = scores["results"][0]["top_scores"]
             scores = [[node_id["v_id"], edge["vertex_id"], edge["score"]] for edge in scores]
-            scores = pd.DataFrame(scores, columns=["node_1","node_2","score"])
+            scores = pd.DataFrame(scores, columns=["node_1", "node_2", "score"])
             all_scores.append(scores)
-    
+
         all_scores = pd.concat(all_scores)
         return all_scores
